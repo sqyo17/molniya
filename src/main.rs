d3303098cb30af302aa6c8e4ffa0a73019b0584a
sqyo <rizkidn17@gmail.com>
2026-01-12 22:49:00 +0700

    Add command on main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+mod cli;
+mod preset;
+mod config;
+
+use clap::Parser;
+use cli::{Cli, Commands};
+
+fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Restore { backup, db, preset } => {}
+        Commands::Preset { action } => {
+            preset::handle(action)?;
+        }
+    }
+
+    Ok(())
 }
